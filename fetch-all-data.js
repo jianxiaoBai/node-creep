e0cfcd012da8916d43075dd1a5f6b16f24074f38
baixiaojian <15101661380@163.com>
2018-05-11 19:13:12 +0800

    去掉了科学计算法

--- fetch-all-data.js
+++ fetch-all-data.js
@@ -1,13 +1,13 @@
-const cheerio = require('cheerio');
-const xlsx = require('node-xlsx').default;
+const cheerio    = require('cheerio');
+const xlsx       = require('node-xlsx').default;
 const superagent = require('superagent');
 const eventproxy = require('eventproxy');
-const fs = require('fs');
-const async = require('async');
-const express = require('express');
-const static = require('express-static');
-const ep = new eventproxy();
-const Decimal = require('decimal.js');
+const fs         = require('fs');
+const async      = require('async');
+const express    = require('express');
+const static     = require('express-static');
+const ep         = new eventproxy();
+const Decimal    = require('decimal.js');
 
 let flag = false;
 
@@ -40,7 +40,7 @@ function init() {
 
   superagent.get('https://etherscan.io/token/generic-tokentxns2?contractAddress=0xa4d17ab1ee0efdd23edc2869e7ba96b89eecf9ab&mode=').end(function (err, res) {
     let $ = cheerio.load(res.text);
-    // page = $('#PagingPanel span').children('b').eq(1).text();
+    page = $('#PagingPanel span').children('b').eq(1).text();
     console.log(`总共${page}页数据`);
 
     for (let i = 1; i <= page; i++) {
@@ -116,10 +116,7 @@ function init() {
           if (!res) return
           console.log(`请求了${++toIndex}次, 占${Math.ceil(toIndex / sumLength * 100)}%，总请求${ sumLength }次`);
           let num = res.body.result;
-          callback(null, [
-            scientificToNumber(num / 1000000000000000000),
-            to
-          ])
+          callback(null, [ num, to ])
         })
     }
   });
